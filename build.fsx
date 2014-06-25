d94abadd492b6712f3e13ff8e1de7822e3d81a5c
Dustin Leavins <dustinleavins@gmail.com>
2014-06-24 20:02:53 -0500

    Various FAKE build file tweaks

--- build.fsx
+++ build.fsx
@@ -6,46 +6,60 @@ open System.Text.RegularExpressions
 open Microsoft.Win32
 open System.Diagnostics
 
-// Default target
-Target "Default" (fun _ ->
-    let nsis = (fun args ->
-        // Lookup registry
-        let cmd = ((Registry.GetValue (@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\NSIS", "", null)).ToString()) + "\\makensis.exe"
+exception NsisError of string
+
+let nsis = (fun args ->
+  // Lookup registry
+  let registry = Registry.GetValue (@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\NSIS", "", null);
         
-        let pInfo = new ProcessStartInfo(cmd, args);
-        pInfo.UseShellExecute <- false
-        pInfo.RedirectStandardError <- true
-        pInfo.RedirectStandardOutput <- true
-        trace ((Process.Start (pInfo)).StandardOutput.ReadToEnd())
-    )
+  if registry = null then raise (NsisError("NSIS is not installed")) 
+        
+  let cmd = ((registry).ToString()) + "\\makensis.exe";
+        
+  let pInfo = new ProcessStartInfo(cmd, args);
+  pInfo.UseShellExecute <- false
+  pInfo.RedirectStandardError <- true
+  pInfo.RedirectStandardOutput <- true
+        
+  let proc = Process.Start (pInfo)
+  trace (proc.StandardOutput.ReadToEnd())
     
-    let pattern = new Regex ("""AssemblyVersion\("(\d+(?:\.\d+){1,3})"\)""")
-    let readLines (filePath:string) = seq {
-        use sr = new StreamReader(filePath)
-        while not sr.EndOfStream do
-            yield sr.ReadLine ()
-    }
+  if proc.ExitCode <> 0 then raise (NsisError("Did not build installer"))
+)
+
+let pattern = new Regex ("""AssemblyVersion\("(\d+(?:\.\d+){1,3})"\)""")
+let readLines (filePath:string) = seq {
+    use sr = new StreamReader(filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
     
-    let hasVersionInfo (line:string) =
-        pattern.IsMatch(line)
+let hasVersionInfo (line:string) =
+    pattern.IsMatch(line)
     
-    let matches = Seq.filter hasVersionInfo (readLines ("SolutionInfo.cs"))
-    let matchCast (captures) : seq<Capture> = Seq.cast captures
-    let versionNumber = (Seq.nth 1 (matchCast (pattern.Match (Seq.nth 0 matches)).Groups)).Value
-    
-    let buildDir = "./Files/" + versionNumber
-    
+let matches = Seq.filter hasVersionInfo (readLines ("SolutionInfo.cs"))
+let matchCast (captures) : seq<Capture> = Seq.cast captures
+let versionNumber = (Seq.nth 1 (matchCast (pattern.Match (Seq.nth 0 matches)).Groups)).Value
+let buildDir = "./Files/" + versionNumber
+
+Target "Clean" (fun _ ->
     CleanDir buildDir
-    
+    File.Delete ("main.exe")
+    File.Delete ("setup.exe")
+)
+
+Target "BuildProduction" (fun _ ->
     !! "LeavinsSoftware.Collection.Program/*.csproj"
       |> MSBuildRelease buildDir "Build"
-      |> Log "AppBuild-Output: "
-      
+      |> Log "Build: "
+)
+
+Target "Default" (fun _ ->
     nsis ("/DProgramVersion=" + versionNumber + " Installer.nsi")
     
     !! "Bootstrapper.proj"
       |> MSBuildRelease "." "BuildBootstrapper"
-      |> Log "AppBuild-Output: "
+      |> Log "Build: "
       
     nsis ("/DProgramVersion=" + versionNumber + " \"Wrapper Installer.nsi\"")
     
@@ -53,6 +67,10 @@ Target "Default" (fun _ ->
     File.Delete ("main.exe")
     File.Delete ("setup.exe")
 )
+
+"Clean"
+  ==> "BuildProduction"
+  ==> "Default"
 
 // start build
 RunTargetOrDefault "Default"
