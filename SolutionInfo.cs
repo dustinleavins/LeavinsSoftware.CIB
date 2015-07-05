f79a02907b11862d92a57d21d9860411f0123c35
Dustin Leavins <dustinleavins@gmail.com>
2015-07-05 16:19:01 -0500

    Update copyright date.

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.9")]
 [assembly: AssemblyCompany("Leavins Software")]
-[assembly: AssemblyCopyright("Copyright © Leavins Software 2013, 2014")]
+[assembly: AssemblyCopyright("Copyright © Leavins Software 2013-2015")]
 [assembly: AssemblyProduct("CIB Collection Manager")]
