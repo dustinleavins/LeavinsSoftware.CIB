b35084c7dd5cbc12f1128fb5fef06bf8f6f68e56
Dustin Leavins <dustinleavins@gmail.com>
2015-07-29 20:29:28 -0500

    Start of sidebar.

---
+++ LeavinsSoftware.Collection.Program/Controls/Sidebar.xaml.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) 2015 Dustin Leavins
+// See the file 'LICENSE.txt' for copying permission.
+using KSMVVM.WPF;
+using LeavinsSoftware.Collection.Models;
+using LeavinsSoftware.Collection.Persistence;
+using LeavinsSoftware.Collection.Program.Controls.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LeavinsSoftware.Collection.Program.Controls
+{
+    /// <summary>
+    /// Interaction logic for Sidebar.xaml
+    /// </summary>
+    public partial class Sidebar : UserControl
+    {
+        public static readonly DependencyProperty FrameProperty =
+            DependencyProperty.Register("Frame", typeof(Frame), typeof(Sidebar));
+
+        public Sidebar()
+        {
+            InitializeComponent();
+            var mainWindow = App.Current.MainWindow as MainWindow;
+            DataContext = new SidebarViewModel(new AppNavImpl(this));
+        }
+
+        public Frame Frame
+        {
+            get
+            {
+                return GetValue(FrameProperty) as Frame;
+            }
+            set
+            {
+                SetValue(FrameProperty, value);
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                return;
+            }
+
+            if (Persistence.GetInstance<ICategoryPersistence>().Any(ItemCategoryType.Product))
+            {
+                productLabel.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                productLabel.Visibility = System.Windows.Visibility.Collapsed;
+            }
+
+            // Comic Book
+            if (Persistence.GetInstance<ICategoryPersistence>().Any(ItemCategoryType.ComicBook))
+            {
+                comicLabel.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                comicLabel.Visibility = System.Windows.Visibility.Collapsed;
+            }
+
+            // Video Game
+            if (Persistence.GetInstance<ICategoryPersistence>().Any(ItemCategoryType.VideoGame))
+            {
+                gameLabel.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                gameLabel.Visibility = System.Windows.Visibility.Collapsed;
+            }
+        }
+
+        private class AppNavImpl : IAppNavigationService
+        {
+            public AppNavImpl(Sidebar instance)
+            {
+                Instance = instance;
+            }
+
+            public Sidebar Instance
+            {
+                get;
+                private set;
+            }
+
+            public Page CurrentPage
+            {
+                get
+                {
+                    return Instance.Frame.Content as Page;
+                }
+            }
+
+            public void GoBack()
+            {
+                if (Instance.Frame.CanGoBack)
+                {
+                    Instance.Frame.GoBack();
+                }
+            }
+
+            public void Navigate<TPage>(Func<TPage> page) where TPage : Page
+            {
+                Instance.Frame.Navigate(page());
+            }
+        }
+    }
+}
---
+++ LeavinsSoftware.Collection.Program/Controls/ViewModels/SidebarViewModel.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2015 Dustin Leavins
+// See the file 'LICENSE.txt' for copying permission.
+using KSMVVM.WPF;
+using KSMVVM.WPF.ViewModel;
+using LeavinsSoftware.Collection.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LeavinsSoftware.Collection.Program.Controls.ViewModels
+{
+    public sealed class SidebarViewModel : ViewModelBase
+    {
+        public SidebarViewModel(IAppNavigationService nav)
+        {
+            Nav = nav;
+
+            ProductCategory = new CustomCommand((unused) =>
+                Nav.Navigate(() => CategoryPage.PageFor<Product>()));
+
+            ComicCategory = new CustomCommand((x) =>
+                Nav.Navigate(() => CategoryPage.PageFor<ComicBookSeries>()));
+
+            GameCategory = new CustomCommand((x) =>
+                Nav.Navigate(() => CategoryPage.PageFor<VideoGame>()));
+        }
+
+        public IAppNavigationService Nav
+        {
+            get;
+            private set;
+        }
+
+        public ICommand ProductCategory { get; private set; }
+
+        public ICommand ComicCategory { get; private set; }
+
+        public ICommand GameCategory { get; private set; }
+    }
+}
