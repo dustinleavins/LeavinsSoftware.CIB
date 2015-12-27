2e63106302e406662c567b180462812541da9379
Dustin Leavins <dustinleavins@gmail.com>
2015-12-27 10:22:37 -0600

    SolutionInfo 0.10

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.9")]
+[assembly: AssemblyVersion("0.10")]
 [assembly: AssemblyCompany("Leavins Software")]
 [assembly: AssemblyCopyright("Copyright © Leavins Software 2013-2015")]
 [assembly: AssemblyProduct("CIB Collection Manager")]
